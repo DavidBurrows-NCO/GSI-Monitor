c2098ee42364889fb70728cdfa607cfd3850f293
David Burrows <David.Burrows@noaa.gov>
2024-10-18 08:05:51 -0400

    update module versions for Gaea-C5 OS updgrade and spack-stack installation

--- modulefiles/gaea.intel.lua
+++ modulefiles/gaea.intel.lua
@@ -3,8 +3,8 @@ help([[
 
 prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
 
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2023.1.0"
-local stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.25"
+local stack_intel_ver=os.getenv("stack_intel_ver") or "2023.2.0"
+local stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.28"
 local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 
 load(pathJoin("stack-intel", stack_intel_ver))
